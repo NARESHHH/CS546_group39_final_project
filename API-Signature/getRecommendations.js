126b78fb122c275eab701c0e37f31626633eed10
VivekSharmaStevens <100751959+VivekSharmaStevens@users.noreply.github.com>
2022-05-04 23:23:15 -0400

    Get Recommendations Intial changes

---
+++ API-Signature/getRecommendations.js
@@ -0,0 +1,15 @@
+const requestBody = {};
+
+const requestQuery = "maxDistance=5";
+
+const responseBody = {
+  data: {
+    displayPicture: "", // base 64
+    firstName: "jhonny",
+    lastName: "depp",
+    description: "I love playing football",
+    interests: ["football", "cricket"],
+    age: 21,
+    gender: "male",
+  },
+};
