a316cbb8b0b59b001bd6521c3167399d2e803bc1
VivekSharmaStevens <100751959+VivekSharmaStevens@users.noreply.github.com>
2022-05-05 19:57:23 -0400

    getAllNotifications API Signature changes

--- API-Signature/getAllNotifications.js
+++ API-Signature/getAllNotifications.js
@@ -4,17 +4,15 @@ const responseBody = {
   data: [
     {
       fromUser: "Vivek",
-      message: "Hello",
       status: "read",
       type: "chat",
     },
     {
       fromUser: "naresh",
-      message: "Naresh requested for a match",
       status: "unread",
       type: "match",
     },
   ],
-  count: 2,
+  total: 2,
   unreadCount: 1,
 };
